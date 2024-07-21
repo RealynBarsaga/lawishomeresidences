4c08475c2fb5abdae99c6779ecab0710683956fa
Gilbert Sevillejo <realynb704@gmail.com>
2024-07-20 19:27:54 -0700

    file upload

--- admin/connection.php
+++ admin/connection.php
@@ -1,12 +1,11 @@
 <?php
 // Database credentials
-$db_server = "localhost";
 $MySQL_username = "u510162695_db_barangay";
 $Password = "1Db_barangay";    
 $MySQL_database_name = "u510162695_db_barangay";
 
 // Establishing connection with server
-$con = mysqli_connect($db_server, $MySQL_username, $Password, $MySQL_database_name);
+$con = mysqli_connect('localhost', $MySQL_username, $Password, $MySQL_database_name);
 
 // Checking connection
 if (!$con) {
