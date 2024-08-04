3c947fbc3c5d60655368890851e6bda96afa3c0d
Gilbert Sevillejo <realynb704@gmail.com>
2024-08-04 00:40:34 -0700

    files upload

--- pagesbun/connection.php
+++ pagesbun/connection.php
@@ -1,5 +1,18 @@
-<?php		
-			// Establishing Connection with Server by passing inputs as a parameter
- $con = mysqli_connect('localhost','root','','db_barangay') or die(mysqli_error());
-			date_default_timezone_set("Asia/Manila");
+<?php
+// Database credentials
+$MySQL_username = "u510162695_db_barangay";
+$Password = "1Db_barangay";    
+$MySQL_database_name = "u510162695_db_barangay";
+
+// Establishing connection with server
+$con = mysqli_connect('localhost', $MySQL_username, $Password, $MySQL_database_name);
+
+// Checking connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Setting the default timezone
+date_default_timezone_set("Asia/Manila");
+
 ?>
