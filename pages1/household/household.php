79b97a5262f16122faafae16aac64d5a92640cf8
Gilbert Sevillejo <realynb704@gmail.com>
2024-08-03 22:18:47 -0700

    files upload

--- pages1/household/household.php
+++ pages1/household/household.php
@@ -13,11 +13,11 @@
 <body class="skin-black">
     <!-- header logo: style can be found in header.less -->
     <?php include "../../../connection.php"; ?>
-    <?php include('../../header.php'); ?>
+    <?php include('../../pages1/header.php'); ?>
 
     <div class="wrapper row-offcanvas row-offcanvas-left">
         <!-- Left side column. contains the logo and sidebar -->
-        <?php include('../../../sidebar-left.php'); ?>
+        <?php include('../../pages1/sidebar-left.php'); ?>
 
         <!-- Right side column. Contains the navbar and content of the page -->
         <aside class="right-side">
