9bc87895ea7644a78c752aa848e08618b2eebe0f
Gilbert Sevillejo <realynb704@gmail.com>
2024-08-03 22:05:29 -0700

    files upload

--- pages1/household/household.php
+++ pages1/household/household.php
@@ -4,7 +4,7 @@
     <?php
     session_start();
     if (!isset($_SESSION['userid'])) {
-        header('Location: ../../pages1/login.php');
+        header('Location: ../../login.php');
         exit; // Ensure no further execution after redirect
     }
     include('../../pages1/head_css.php'); // Removed ob_start() since it's not needed here
