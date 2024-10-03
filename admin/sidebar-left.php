5aee5a9d18313394c29fb1c046efc11f8dc65885
Gilbert Sevillejo <realynb704@gmail.com>
2024-10-04 03:44:50 +0800

    files upload

--- admin/sidebar-left.php
+++ admin/sidebar-left.php
@@ -1,43 +1,43 @@
 <?php
-    echo '
-    <aside class="left-side sidebar-offcanvas">
-        <!-- sidebar: style can be found in sidebar.less -->
-        <section class="sidebar">
-            <!-- sidebar menu: : style can be found in sidebar.less -->
-            <ul class="sidebar-menu">
-                <li>
-                    <a href="/dashboard">
-                        <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/officials">
-                        <i class="fa fa-user"></i> <span>Madridejos Officials</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/staff">
-                        <i class="fa fa-university"></i> <span>List Of Barangay</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/householdlist">
-                        <i class="fa fa-users"></i> <span>List Of Household</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/permit">
-                        <i class="fa fa-file"></i> <span>Permit</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/generatereports">
-                        <i class="fa fa-paper-plane"></i> <span>Generated Reports</span>
-                    </a>
-                </li>                  
-            </ul>
-        </section>
-        <!-- /.sidebar -->
-    </aside>
-    ';
+	echo '
+	<aside class="left-side sidebar-offcanvas">
+                <!-- sidebar: style can be found in sidebar.less -->
+                <section class="sidebar">
+                    <!-- sidebar menu: : style can be found in sidebar.less -->
+                    <ul class="sidebar-menu">
+                            <li>
+                                <a href="../../admin/dashboard/dashboard.php?page=dashboard">
+                                    <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="../../admin/officials/officials.php?page=officials">
+                                    <i class="fa fa-user"></i> <span>Madridejos Officials</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="../../admin/staff/staff.php?page=staff">
+                                    <i class="fa fa-university"></i> <span>List Of Barangay</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="../householdlist/householdlist.php?page=householdlist">
+                                    <i class="fa fa-users"></i> <span>List Of Household</span>
+                                </a>
+                            </li>
+                             <li>
+                                <a href="../permit/permit.php?page=permit">
+                                    <i class="fa fa-file"></i> <span>Permit</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="../generatereports/generatereports.php">
+                                    <i class="fa fa-paper-plane"></i> <span>Generated Reports</span>
+                                </a>
+                            </li>                  
+                    </ul>
+                </section>
+                <!-- /.sidebar -->
+            </aside>
+	';
 ?>
