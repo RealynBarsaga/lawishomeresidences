a1b110d6bdd132eb5310d24fd02e5fafe2e60d77
Gilbert Sevillejo <realynb704@gmail.com>
2024-10-04 11:08:07 +0800

    files upload

--- admin/login.php
+++ admin/login.php
@@ -596,7 +596,7 @@ html {
                         // Attach a click event to the OK button
                         document.getElementById("ok-button").addEventListener("click", function() {
                             // Redirect to the dashboard after the OK button is clicked
-                            window.location.href = '/admin/dashboard';
+                            window.location.href = '../admin/dashboard/dashboard.php?page=dashboard';
                         });
                     });
                 </script>
