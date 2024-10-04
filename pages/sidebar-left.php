0990b8e20660017a42ae153f1410539be269eef2
Gilbert Sevillejo <realynb704@gmail.com>
2024-10-04 11:02:25 +0800

    files upload

--- pages/sidebar-left.php
+++ pages/sidebar-left.php
@@ -6,37 +6,37 @@
                     <!-- sidebar menu: : style can be found in sidebar.less -->
                         <ul class="sidebar-menu">
                             <li>
-                                <a href="pages/dashboard/dashboard.php?page=dashboard">
+                                <a href="../dashboard/dashboard.php?page=dashboard">
                                     <i class="fa fa-dashboard"></i> <span>Dashboard</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="pages/officials/officials.php?page=officials">
+                                <a href="../officials/officials.php?page=officials">
                                     <i class="fa fa-user"></i> <span>Barangay Officials</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="pages/household/household.php?page=household">
+                                <a href="../household/household.php?page=household">
                                     <i class="fa fa-home"></i> <span>Household</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="pages/resident/resident.php?page=resident">
+                                <a href="../resident/resident.php?page=resident">
                                     <i class="fa fa-users"></i> <span>Resident</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="pages/brgyclearance/brgyclearance.php?page=brgyclearance">
+                                <a href="../brgyclearance/brgyclearance.php?page=brgyclearance">
                                     <i class="fa fa-file"></i> <span>Barangay Clearance</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="pages/certofresidency/certofres.php?page=certofresidency">
+                                <a href="../certofresidency/certofres.php?page=certofresidency">
                                     <i class="fa fa-file"></i> <span>Certificate Of Residency</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="pages/certofindigency/certofindigency.php?page=certofindigency">
+                                <a href="../certofindigency/certofindigency.php?page=certofindigency">
                                     <i class="fa fa-file"></i> <span>Certificate Of Indigency</span>
                                 </a>
                             </li>
