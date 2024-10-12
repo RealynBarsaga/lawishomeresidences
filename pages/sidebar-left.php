730706a905cef6692d7a51eb3e2d8dff8efc42f3
Gilbert Sevillejo <realynb704@gmail.com>
2024-10-12 18:47:23 +0800

    files upload

--- pages/sidebar-left.php
+++ pages/sidebar-left.php
@@ -26,7 +26,7 @@
                                 </a>
                             </li>
                             <li>
-                                <a href="brgyclearance/brgyclearance.php?page=brgyclearance">
+                                <a href="../brgyclearance/brgyclearance.php?page=brgyclearance">
                                     <i class="fa fa-file"></i> <span>Barangay Clearance</span>
                                 </a>
                             </li>
